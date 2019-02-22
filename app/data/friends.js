22f645b56def00221573a0d90228661bc0a5653d
Morgan Garcia <morgangarcia1977@gmail.com>
2019-02-22 17:42:19 -0500

    secondpush

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+const friends = [ {
+    "name" : "Josh",
+    "photo" : "",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Sherri",
+    "photo" : "",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Kirill",
+    "photo" : "",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Amy",
+    "photo" : "",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Gavin",
+    "photo" : "",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Kayla",
+    "photo" : "",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "PJ",
+    "photo" : "",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Janie",
+    "photo" : "",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Andre",
+    "photo" : "",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Krissy",
+    "photo" : "",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Zob",
+    "photo" : "",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Danielle",
+    "photo" : "",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Charu",
+    "photo" : "",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Omar",
+    "photo" : "",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
